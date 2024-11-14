571c65686e6e0c746436222113185c198bba237f
Giorgi Oniani <giorgi.oniani.04@gmaio.com>
2024-11-15 00:23:40 +0400

    - Add Dish and Bar item forms

--- restaurant_admin/forms.py
+++ restaurant_admin/forms.py
@@ -8,8 +8,20 @@ class DishCategoriesForm(forms.ModelForm):
         fields = ['dish_category_name', 'dish_category_description', 'is_active_dish_category', 'dish_category_photo']
 
 
-
 class BarCategoriesForm(forms.ModelForm):
     class Meta:
         model = BarCategory
         fields = ['bar_category_name', 'bar_category_description', 'is_bar_category_active', 'bar_category_photo']
+
+
+class DishItemsForm(forms.ModelForm):
+    class Meta:
+        model = DishItem
+        fields = ['dish_item_name', 'dish_item_description', 'dish_item_weight', 'is_allergenic_dish_item',
+                  'dish_item_price', 'dish_item_cooking_time', 'is_active_dish_item', 'dish_item_photo']
+
+class BarItemsForm(forms.ModelForm):
+    class Meta:
+        model = BarItem
+        fields = ['bar_item_name', 'bar_item_description', 'bar_item_weight', 'is_allergenic_bar_item',
+                  'bar_item_price', 'bar_item_cooking_time', 'is_active_bar_item', 'bar_item_photo']
--- restaurant_admin/views.py
+++ restaurant_admin/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from django.views.generic import ListView, TemplateView, DetailView
 from unicodedata import category
 from .models import RestaurantInfo, DishCategory, DishItem, Currency, BarCategory, BarItem, User
-from .forms import DishCategoriesForm, BarCategoriesForm
+from .forms import DishCategoriesForm, BarCategoriesForm, BarItemsForm, DishItemsForm
 
 
 class AdminMainPageView(ListView):
@@ -37,12 +37,26 @@ class AdminMainPageView(ListView):
 class DishCategoryDetailView(DetailView):
     model = DishCategory
     template_name = 'restaurant_admin/dish-category-detail.html'
-    context_object_name = 'dish_category'
+    context_object_name = 'dish_items'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['dish_items'] = DishItem.objects.filter(category=self.object)
+        context['form'] = DishItemsForm()
         return context
+
+    def post(self, request, *args, **kwargs):
+        form = DishItemsForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_item = form.save(commit=False)
+            new_item.category = self.get_object()
+            new_item.restaurant = RestaurantInfo.objects.get(restaurant_owner=request.user)
+            new_item.save()
+            return redirect('dish-category-detail', slug=self.get_object().slug)
+        else:
+            context = self.get_context_data()
+            context['form'] = form
+            return self.render_to_response(context)
 
 
 class BarCategoryView(ListView):
@@ -61,7 +75,7 @@ class BarCategoryView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['form'] = BarCategoriesForm
+        context['form'] = BarCategoriesForm()
         return context
 
     def post(self, request, *args):
@@ -78,10 +92,23 @@ class BarCategoryView(ListView):
 class BarCategoryDetailView(DetailView):
     model = BarCategory
     template_name = 'restaurant_admin/bar-category-detail.html'
-    context_object_name = 'bar_category'
+    context_object_name = 'bar_items'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['bar_items'] = BarItem.objects.filter(category=self.object)
+        context['form'] = BarItemsForm()
         return context
 
+    def post(self, request, *args, **kwargs):
+        form = BarItemsForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_item = form.save(commit=False)
+            new_item.category = self.get_object()
+            new_item.restaurant = RestaurantInfo.objects.get(restaurant_owner=request.user)
+            new_item.save()
+            return redirect('bar-category-detail', slug=self.get_object().slug)
+        else:
+            context = self.get_context_data()
+            context['form'] = form
+            return self.render_to_response(context)
